f4a6980dc7d97d7fee7df72646c3df2da450dced
Michael Zhong <mzhong@org-m-mzhong.fios-router.home>
2017-07-10 08:16:05 -0400

    replaced some icons and lots of copy / minor styling changes

--- src/components/Action/index.js
+++ src/components/Action/index.js
@@ -21,11 +21,9 @@ const Action = (props) => {
         <div className="action_content">
           <h3 className="action_headline">{props.headline}</h3>
           <p className="action_text">{props.text}</p>
-        </div>
-      </div>
-      <div className="action_form">
-        <div className="action_form_box">
-          {props.children}
+          <div className="action_form">
+            {props.children}
+          </div>
         </div>
       </div>
     </div>
--- src/containers/ProblemsPage/index.js
+++ src/containers/ProblemsPage/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.less';
 import ProblemsSection from '../ProblemsSection';
-const introImg = require('../../assets/credit-scene.svg');
+const introImg = require('../../assets/credit-icon.gif');
 
 const ProblemsPage = () => {
   return (
@@ -9,8 +9,8 @@ const ProblemsPage = () => {
       <div className="intro_main_section">
         <img className="intro_image" src={introImg}/>
         <div className="intro_main_intro">
-          <h1 className="intro_headline">I Got Denied</h1>
-          <p className="intro_tagline">If you're having a hard time qualifying for a loan or getting an apartment you want, your credit could be to blame. But it's within your power to build your credit profile and unlock financial possibilities.</p>
+          <h1 className="intro_headline">My Credit is Keeping Me from My Goal</h1>
+          <p className="intro_tagline">If you're having a hard time qualifying for a loan or getting an apartment you want, you can take steps to improve your credit. Let's take a look at what's possible.</p>
         </div>
       </div>
       <ProblemsSection />
